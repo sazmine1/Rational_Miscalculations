29a26aff64d888854405390a95bddea388d51286
NancyLettuce <nli4@stuy.edu>
2015-11-18 22:07:29 -0500

    PHASE 2 COMPLETE

--- Rational.java
+++ Rational.java
@@ -54,11 +54,17 @@ public class Rational {
         p = p*s.getQ();
         q = q*s.getP();
     }
+    
+
+    //////////////////////     PHASE 2    ///////////////////
+	  
     //adds two rational classes and changes value of initial class
     public void add(Rational s){
         p = (this.getP() * s.getQ()) + (s.getP() * this.getQ());
-        q = this.getQ() * s.getQ();
+	//numerators multiplied by the other rational object's denominator then added
+        q = this.getQ() * s.getQ(); 
     }
+    
     //subtracts two rational classes and changes value of initial class
     public void subtract(Rational s){
         p = (this.getP() * s.getQ()) - (s.getP() * this.getQ());
@@ -70,33 +76,38 @@ public class Rational {
         if ((a == b) || (b == 0)){
             return a;} //returns the GCD.
         else if (a < b){
-            return gcdER (b,a);} //If b is greater than a, the function will be run again with both values swapped.
+            return gcdER (b,a);
+	} //If b is greater than a,the function will be run again with both values swapped.
         else{
             return gcdER (b , (a-b));
         }
     } //Recalls the function with b and the difference between a and b.
+    
     //changes rational to reduced form
     public void reduce() {
 	int p1 = p;
 	int q1 = q;
-        p = p / gcdER(p1, q1);
-        q = q / gcdER(p1, q1);
-    } 
+        p = p / gcdER(p1, q1); //num divided by gcd of num and denom
+        q = q / gcdER(p1, q1); //denom divided by gcd of num and denom
+    }
+
+
+    
     //main method for testing purposes
     public static void main(String[] args){
         Rational r = new Rational(2,3); //Stores the rational number 2/3
         Rational s = new Rational(1,2); //Stores the rational number 1/2
         System.out.println(r);
         System.out.println(s);	
-	r.multiply(s); //Multiplies r by s, changes r to 2/6.  s remains Â½
+	r.multiply(s); //Multiplies r by s, changes r to 2/6.  s remains ½
         System.out.println(r);
-        r.divide(s); //Multiplies r by s, changes r to 2/6.  s remains Â½
+        r.divide(s); //Multiplies r by s, changes r to 2/6.  s remains ½
         System.out.println(r);
-	r.add(s);
+	r.add(s); //Adds r and s, r becomes 14/12
         System.out.println(r);
-	r.subtract(s);
+	r.subtract(s); //subtracts s from r, r becomes 16/24
         System.out.println(r);
-	r.reduce();
+	r.reduce(); // r becomes 2/3
         System.out.println(r);	
         Rational a = new Rational(1,0);
         System.out.println(a);
