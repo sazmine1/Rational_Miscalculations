8ae441b575799c84205e7c5685f43fa485064dfc
sazmine1 <sazmine@stuy.edu>
2015-11-24 20:04:29 -0500

    Update Rational.java

--- Rational.java
+++ Rational.java
@@ -1,8 +1,8 @@
 /* 
-Team Miscalculations - Nancy Li, Sadia Azmine
-APCS1 pd9
-HW 33 -- Do You Even Add, Bro?
-2015-11-19
+   Team Miscalculations - Nancy Li, Sadia Azmine
+   APCS1 pd9
+   HW 33 -- Do You Even Add, Bro?
+   2015-11-19
 */
 
 public class Rational {
@@ -120,7 +120,7 @@ public class Rational {
 	    return -1;
     	}//Returns -1 if the calling number is smaller than the parameter
     }
-       //////////////////////     PHASE 4     ///////////////////
+    //////////////////////     PHASE 4     ///////////////////
     public boolean equals( Object a ) {
  
 	//First, check for aliasing.
@@ -129,14 +129,14 @@ public class Rational {
 	//Next, if this and input Object are different objects,
         if ( !retVal ) {
  
-		//...check to see if input Object is a Tile
-		if (retVal = a instanceof Rational) {
-			this.reduce();
-			((Rational)a).reduce();
+	    //...check to see if input Object is a Tile
+	    if (retVal = a instanceof Rational) {
+		this.reduce();
+		((Rational)a).reduce();
 			
-		        //...and that its state variables match those of this Tile
-		        retVal = (this.getP() == ((Rational)a).getP()) && (this.getQ() == ((Rational)a).getQ());
-	        }
+		//...and that its state variables match those of this Tile
+		retVal = (this.getP() == ((Rational)a).getP()) && (this.getQ() == ((Rational)a).getQ());
+	    }
         }
 	return retVal;
     }
@@ -147,29 +147,52 @@ public class Rational {
     public static void main(String[] args){
         Rational r = new Rational(2,3); //Stores the rational number 2/3
         Rational s = new Rational(1,2); //Stores the rational number 1/2
-        System.out.println(r);
-        System.out.println(s);
-	
-	//tests Phase 3
-        System.out.println(r.compareTo(r)); //should return 0
-	System.out.println(r.compareTo(s)); //should return 1
-	System.out.println(s.compareTo(r)); //should return -1
+        System.out.println("Rational r: \t" + r);
+        System.out.println("Rational s: \t" + s);
 
 	//tests Phase 1
+	System.out.println("Testing Phase 1...");	
 	r.multiply(s); //Multiplies r by s, changes r to 2/6.  s remains ½
-        System.out.println(r);
-        r.divide(s); //Multiplies r by s, changes r to 4/6.  s remains ½
-        System.out.println(r);
+	System.out.println("r * s = \t" + r);
+	System.out.println("r now equals 2/6");	
+	r.divide(s); //Multiplies r by s, changes r to 4/6.  s remains ½
+	System.out.println("r / s = \t" + r);
+	System.out.println("r now equals 4/6");	
 
 	//tests Phase 2
+	System.out.println("Testing Phase 2...");	
 	r.add(s); //Adds r and s, r becomes 14/12
-        System.out.println(r);
+	System.out.println("r + s = \t" + r);
+	System.out.println("r now equals 14/12");	
 	r.subtract(s); //subtracts s from r, r becomes 16/24
-        System.out.println(r);
+	System.out.println("r - s = \t" + r);
+	System.out.println("r now equals 16/24");	
 	r.reduce(); // r becomes 2/3
-        System.out.println(r);	
-        Rational a = new Rational(1,0);
-        System.out.println(a);
+	System.out.println("when reduced r now equals: \t" + r);	
+	Rational a = new Rational(1,0);
+	System.out.println(a);
+		
+	//tests Phase 3
+	System.out.println("Testing Phase 3...");
+	System.out.println("A 0 indicates two Rationals are equal.");
+        System.out.println("How does r compare to r? \t" + r.compareTo(r)); //should return 0
+	System.out.println("A 1 indicates that the first Rational is greater than the second.");
+	System.out.println("How does r compare to s? \t" + r.compareTo(s)); //should return 1
+	System.out.println("A -1 indicates that the first Rational is less than the second.");
+	System.out.println("How does s compare to r? \t" + s.compareTo(r)); //should return -1
+
+	//tests Phase 4
+	System.out.println("Testing Phase 4...");	
+	Rational i = new Rational(1,4);
+	Rational j = new Rational(2,8);
+	System.out.println("Is r equal to s? \t" + r.equals(s));
+        System.out.println("Rational i: \t" + i);
+        System.out.println("Rational j: \t" + j);	
+	System.out.println("Is i equal to j? \t" + i.equals(j));		
+	System.out.println("Is i equal to i? \t" + i.equals(i));	
+	System.out.println("Is j equal to i? \t" + j.equals(i));
+	System.out.println("Rational a is not defined but has a numerator of 1 and denominator of 0.");
+	System.out.println("Is i equal to a? \t" + i.equals(a));
     }
 } 
 //end class Rational
